13c7213502f0f3d65c3a459797b9f962ac392c3f
Kyle Grealis <kyleGrealis@icloud.com>
2023-11-05 14:35:32 -0600

    feat: data module

---
+++ app/logic/uneq_t_test/data_mod.R
@@ -0,0 +1,36 @@
+box::use(
+  shiny[moduleServer, NS, tagList, reactive, renderUI, uiOutput, req],
+)
+
+box::use(
+  app/logic/uneq_t_test/functions
+)
+
+#' @export
+ui <- function(id) {
+  ns <- NS(id)
+  
+}
+
+#' @export
+server <- function(id, inputs){
+  moduleServer(id, function(input, output, session) {
+    
+    data <- reactive({
+      req(inputs()$alpha)
+      req(inputs()$effect)
+      req(inputs()$group1_n)
+      req(inputs()$group2_n)
+      req(inputs()$alt)
+      
+      functions$t2n_table(
+        inputs()$alpha, inputs()$effect, 
+        inputs()$group1_n, inputs()$group2_n, 
+        inputs()$alt
+      )
+    })
+    
+    return(data)
+    
+  })
+}
