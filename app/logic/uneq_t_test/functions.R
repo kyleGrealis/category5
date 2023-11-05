053f77ac78c46571ffe825d09baa9d1b9a4e4d61
Kyle Grealis <kyleGrealis@icloud.com>
2023-11-05 15:28:52 -0600

    adding logic for comparison cards

--- app/logic/uneq_t_test/functions.R
+++ app/logic/uneq_t_test/functions.R
@@ -15,11 +15,11 @@ box::use(
 #' @export
 t2n_table <- function(alpha, d, n1, n2, alt) {
   # set lowest value as 32 because # obs in n1 must be at least 2
-  if (n1 < 32) {
-    n1 = 32
+  if (n1<32) {
+    n1=32
   }
-  if (n2 < 32) {
-    n2 = 32
+  if (n2<32) {
+    n2=32
   }
   
   expand.grid(
@@ -40,4 +40,45 @@ t2n_table <- function(alpha, d, n1, n2, alt) {
       )$power,
       power=round(power, 2)
     )
+}
+
+
+# plot1
+
+# plot2
+
+
+# this function will calculate the sample size at 80% power and user's inputs
+#' @export
+min_sample <- function(n1, alt, d, alpha) {
+  # round down to maintain mininum group diff
+  floor(
+    pwr::pwr.t2n.test(
+      n1 = n1,
+      n2 = NULL,
+      d = 0.5,
+      sig.level = 0.5,
+      power = 0.8,
+      alternative = "two.sided"
+    )$n2
+  )
+}
+
+
+# this function is used to compare the user's vs calculated min sample size
+#' @export
+t2n_compare <- function(alpha, d, group1_n, group2_n, alt) {
+  compare <- pwr::pwr.t2n.test(
+    sig.level = alpha,
+    d = d,
+    n1 = group1_n,
+    n2 = group2_n,
+    alternative = alt,
+    power = NULL
+  )
+  if (compare$power < 0.8) {
+    return("TOO LOW!")
+  } else {
+    return("Good!")
+  }
 }
