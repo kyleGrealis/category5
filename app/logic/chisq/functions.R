2a4402edf6eefc833b1e8cc062c6b959d46265bb
Kyle Grealis <kyleGrealis@icloud.com>
2023-11-09 13:00:45 -0600

    feat: added plotting functions

--- app/logic/chisq/functions.R
+++ app/logic/chisq/functions.R
@@ -27,17 +27,75 @@ chisq_table <- function(alpha, effect, n, df) {
         power=NULL
       )$power,
       power=round(power, 2)
+    )
 }
 
-# plot 1
+# this is the left plot: power vs sample size
+#' @export
+power_effect <- function(data, n) {
+  # show selected sample size and 30 above and 30 below, min=5
+  if (n-30 < 5) {
+    sample_groups <- c(5, n, n+30)
+  } else {
+    sample_groups <- c(n-30, n, n+30)
+  }
+  data |>
+    filter(n %in% sample_groups) |>
+    group_by(n) |>
+    e_charts(w) |>
+    e_line(power) |>
+    e_tooltip(
+      trigger="item",
+      formatter=left_label_formatter
+    ) |>
+    e_grid(right='15%') |>
+    e_color(c("#f47321", "#777777", "#005030")) |>
+    e_legend(
+      left='5',
+      title=list("Sample size")
+    ) |>
+    e_datazoom(type='inside') |>
+    e_axis_labels(x="Effect \nSize", y="Power") |>
+    e_toolbox_feature(feature=c("saveAsImage"))
+}
 
-
-# plot 2
-
+ # this is the bar chart: power at the 3 effect sizes for the user-selected sample size
+#' @export
+power_bar <- function(data, n) {
+  # plotting the selected power for sample size at 3 levels of effect size
+  data |>
+    filter(
+      n == n,
+      d %in% effect_table$t_test
+    ) |>
+    mutate(
+      # custom x-axis labels
+      d=factor(d, labels=c("Small", "Medium", "Large")),
+      # custom bar color
+      color=case_when(
+        d == "Small" ~ "#f47321",
+        d == "Medium" ~ "#f3f3f3",
+        d == "Large" ~ "#005030"
+      )
+    ) |>
+    e_charts(d) |>
+    e_bar(power) |>
+    e_add_nested("itemStyle", color) |>
+    e_tooltip(
+      trigger="item",
+      # formatter=right_label_formatter
+    ) |>
+    e_grid(right='15%') |>
+    e_color("#005030") |>
+    e_legend(show=FALSE) |>
+    e_datazoom(type='inside') |>
+    e_axis_labels(x="Effect \nSize", y="Power") |>
+    e_toolbox_feature(feature=c("saveAsImage"))
+}
 
 # this function will calculate the sample size at 80% power and user's inputs
 #' @export
-min_sample <- function(alpha, effect, n, df) {
+min_sample <- function(alpha, w, df) {
   # round up to the next whole person with `ceiling`
   ceiling(
     pwr::pwr.chisq.test(
@@ -52,13 +110,13 @@ min_sample <- function(alpha, effect, n, df) {
 
 # this function is used to compare the user's vs calculated min sample size
 #' @export
-chisq_compare <- function(t_type, alt, n, d, alpha) {
+chisq_compare <- function(n, w, alpha) {
   compare <- pwr::pwr.chisq.test(
     sig.level=alpha,
-      df=df,
-      w=w,
-      N=n,
-      power=NULL
+    df=df,
+    w=w,
+    N=n,
+    power=NULL
   )
   if (compare$power < 0.8) {
     return("TOO LOW!")
