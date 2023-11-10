2c6949c40df85a5ed66813dc72758c87605a62f5
Kyle Grealis <kyleGrealis@icloud.com>
2023-11-09 19:11:28 -0600

    feat: added anova modules

---
+++ app/view/anova/leftPlot_mod.R
@@ -0,0 +1,33 @@
+box::use(
+  shiny[moduleServer, NS, tagList, reactive, renderUI, uiOutput],
+)
+
+box::use(
+  app/logic/plotCard,
+  app/logic/anova/functions,
+)
+
+#' @export
+ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    uiOutput(ns("leftPlot"))
+  )
+  
+}
+
+#' @export
+server <- function(id, data, inputs){
+  moduleServer(id, function(input, output, session) {
+
+    plot <- reactive({ functions$power_effect(data=data(), n=inputs()$n) })
+
+    output$leftPlot <- renderUI({
+      plotCard$plotting_cards(
+        "Lines represent group tests (±30) plotted
+        against power and effect size",
+        plot()
+      )
+    })
+  })
+}
