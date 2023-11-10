8cb96d333e4554a923be43a3f94bb657940211e6
Kyle Grealis <kyleGrealis@icloud.com>
2023-11-09 23:42:02 -0600

    corr modules added

---
+++ app/view/corr/inputs_mod.R
@@ -0,0 +1,57 @@
+box::use( 
+  app/view/helpMe_mod
+)
+
+box::use(
+  shiny[h6, moduleServer, NS, numericInput, radioButtons, reactive,
+        selectInput, tagList],
+)
+
+ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    selectInput(
+      ns("alpha"), "Significance level",
+      choices=c(0.01, 0.025, 0.05),
+      selected=0.05
+    ),
+    radioButtons(
+      inputId=ns("effect"),
+      label="Hypothesis correlation (r)",
+      selected=0.3, # medium effect
+      choiceNames=list(
+        "Small (0.1)", "Medium (0.3)", "Large (0.5)"
+      ),
+      choiceValues=list(0.1, 0.3, 0.5)
+    ),
+    helpMe_mod$ui(ns("help")),
+    numericInput(
+      ns("n"), "Number of observations",
+      min=10, max=1300, value=150, step=10
+    ),
+    selectInput(
+      ns("alt"), "Alternative hypothesis type",
+      choices=c(
+        "Two-sided"="two.sided",
+        "Greater than the null"="greater"
+      ),
+      selected="two.sided"
+    )
+  )
+}
+
+server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    
+    helpMe_mod$server("help")
+    
+    reactive({
+      list(
+        alpha  = as.numeric(input$alpha),
+        effect = as.numeric(input$effect),
+        n      = input$n,
+        alt    = input$alt
+      )
+    })
+  })
+}
