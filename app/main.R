008c738a49b817e787f7be042abbb5334f3ade53
Kyle Grealis <84798289+kyleGrealis@users.noreply.github.com>
2024-02-16 18:26:51 -0600

    edit: redirecting!

--- app/main.R
+++ app/main.R
@@ -1,76 +1,33 @@
 box::use(
   bslib[page_navbar, navset_underline, nav_spacer, nav_menu,
         nav_panel, layout_sidebar],
-  shiny[div, icon, markdown, moduleServer, NS],
+  shiny[div, icon, markdown, moduleServer, NS, tags, fluidPage, checkboxInput, observeEvent],
+  shinyjs[runjs, useShinyjs],
 )
 
-box::use(
-  app/view/t_test/t_test,
-  app/view/uneq_t_test/uneq_t_test,
-  app/view/one_samp_prop/one_samp_prop,
-  app/view/two_samp_prop/two_samp_prop,
-  app/view/uneq_samp_prop/uneq_samp_prop,
-  app/view/anova/anova,
-  app/view/corr/corr,
-  app/view/chisq/chisq,
-  app/view/glm/glm,
-  
-  app/view/footer,
-  
-  app/logic/helpMe
-)
+
+jscode <- "Shiny.addCustomMessageHandler('mymessage', function(message) {window.location = 'https://kylegrealis.shinyapps.io/powerViz/';});"
+
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  page_navbar(
-    title = "PowerViz: Power Calculation and Visualization Tool",
-    underline = TRUE,
-    div(
-      style = "margin: 0 auto; max-width: 1300px;",
-      navset_underline(
-        nav_spacer(),
-        nav_menu(
-          title = "t-test",
-          align = "right",
-          t_test$ui(ns("t_test")),
-          uneq_t_test$ui(ns("uneq_t_test"))
-        ),
-        nav_menu(
-          title = "Proportions",
-          align = "right",
-          one_samp_prop$ui(ns("one_samp_prop")),
-          two_samp_prop$ui(ns("two_samp_prop")),
-          uneq_samp_prop$ui(ns("uneq_samp_prop")),
-        ),
-        anova$ui(ns("anova")),
-        corr$ui(ns("corr")),
-        chisq$ui(ns("chisq")),
-        glm$ui(ns("glm")),
-        nav_panel(
-          class = "markdown-panel",
-          shiny::icon("circle-info"), 
-          helpMe$helpMe
-        )
-      )
-    ),
-    div(footer$footer)
+  fluidPage(
+    tags$head(tags$script(jscode)),     
+    checkboxInput(ns("Redirect"), "Click this to go to our new location: 
+                  kyleGrealis.shinyapps.io/powerViz/", value = T)
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    
-    t_test$server("t_test")
-    uneq_t_test$server("uneq_t_test")
-    one_samp_prop$server("one_samp_prop")
-    two_samp_prop$server("two_samp_prop")
-    uneq_samp_prop$server("uneq_samp_prop")
-    anova$server("anova")
-    corr$server("corr")
-    chisq$server("chisq")
-    glm$server("glm")
+  
+    observeEvent(input$Redirect,{
+      if(!input$Redirect){
+        session$sendCustomMessage("mymessage", "mymessage")
+      }
+    })
     
   })
 }
